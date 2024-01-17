209353deafe842c6ba1889683745f159053317d8
Sonnet-Songbird <147113185+Sonnet-Songbird@users.noreply.github.com>
2024-01-17 13:53:51 +0900

    Initial commit

---
+++ src/main/java/org/recorder/nightfactory/NightFactoryApplication.java
@@ -0,0 +1,13 @@
+package org.recorder.nightfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NightFactoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/recorder/nightfactory/NightFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.recorder.nightfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
