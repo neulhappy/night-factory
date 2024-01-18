30a8501464a2a7046490bf1b767bd493ff47ff63
Sonnet-Songbird <147113185+Sonnet-Songbird@users.noreply.github.com>
2024-01-18 16:44:42 +0900

    ADDED: ReservationApiController.java scheduleListResponse.java

---
+++ src/main/java/org/recorder/nightfactory/controller/ReservationApiController.java
@@ -0,0 +1,14 @@
+package org.recorder.nightfactory.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ReservationApiController {
+//
+//    @GetMapping("/api/schedule")
+//    public ResponseEntity<>
+}
