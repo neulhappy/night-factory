3f40354a910952b695c496cd8c44907d54e19886
Yeo-ok <wls990728@naver.com>
2024-01-18 14:18:44 +0900

    [fix] ReservationRepository reservationName -> Owner 수정 오류 수정

--- src/main/java/org/recorder/nightfactory/repository/ReservationRepository.java
+++ src/main/java/org/recorder/nightfactory/repository/ReservationRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-    Optional<Reservation> findByReservationName(String reservationName);
+    Optional<Reservation> findByOwner(String Owner);
 }
