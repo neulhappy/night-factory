809cb7ab8d705a6d2f8da56bdc8c0f79de7fabe0
Yeo-ok <wls990728@naver.com>
2024-01-18 15:16:50 +0900

    [feat] reservation 예약자 이름으로 예약자 정보구하기

--- src/main/java/org/recorder/nightfactory/repository/ReservationRepository.java
+++ src/main/java/org/recorder/nightfactory/repository/ReservationRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-    Optional<Reservation> findByOwner(String Owner);
+    Optional<Reservation> findByOwnerOrderByReservationDateDesc(String owner);
 }
--- src/main/java/org/recorder/nightfactory/service/ReservationDetailService.java
+++ src/main/java/org/recorder/nightfactory/service/ReservationDetailService.java
@@ -1,8 +1,12 @@
 package org.recorder.nightfactory.service;
 
 import lombok.RequiredArgsConstructor;
+import org.recorder.nightfactory.domain.Reservation;
 import org.recorder.nightfactory.repository.ReservationRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -10,6 +14,8 @@ public class ReservationDetailService {
 
     private final ReservationRepository reservationRepository;
     //예약자 이름으로 예약 정보 가져오기
-
+    public Optional<Reservation> findByOwner(String owner){
+        return reservationRepository.findByOwnerOrderByReservationDateDesc(owner);
+    }
 
 }
