9e1bd06b0795fd38037020cd38bade71f6452d12
Sonnet-Songbird <147113185+Sonnet-Songbird@users.noreply.github.com>
2024-01-25 11:08:48 +0900

    REFACTOR: Merge Class.Domain Payment to Reservation

--- docs/ddl.sql
+++ docs/ddl.sql
@@ -12,19 +12,24 @@ CREATE TABLE IF NOT EXISTS theme
 
 CREATE TABLE IF NOT EXISTS schedule
 (
-    id        INT PRIMARY KEY auto_increment,
-    room_id   INT REFERENCES theme (room_id) NOT NULL,
+    id         INT PRIMARY KEY auto_increment,
+    room_id    INT REFERENCES theme (room_id) NOT NULL,
     start_time TIME                           NOT NULL UNIQUE
 );
 
 
 CREATE TABLE IF NOT EXISTS reservations
 (
-    id              BINARY(16) PRIMARY KEY auto_increment,
-    scheduleId      INT REFERENCES schedule (room_id) NOT NULL,
-    owner           VARCHAR                           NOT NULL,
-    reservationDate DATE                              NOT NULL,
-    phoneNumber     VARCHAR                           NOT NULL,
-    numberOfPeople  INT                               NOT NULL,
-    reservationAt   DATE
-)
+    id              BINARY(16) PRIMARY KEY,
+    scheduleId      INT REFERENCES schedule (id) NOT NULL,
+    owner           VARCHAR                      NOT NULL,
+    reservationDate DATE                         NOT NULL,
+    phoneNumber     VARCHAR                      NOT NULL,
+    numberOfPeople  INT                          NOT NULL,
+    reservationAt   TIMESTAMP                    NOT NULL,
+    amount          INT                          NOT NULL,
+    payment_id      BINARY(16) COMMENT '결제 요청시 가맹점에서 채번하는 결제 아이디',
+    imp_uid         VARCHAR COMMENT '결제 완료시 OnePort에서 채번 하는 결제 아이디',
+    payment_state   VARCHAR,
+    paid_at         TIMESTAMP
+);
--- src/main/java/org/recorder/nightfactory/domain/Reservation.java
+++ src/main/java/org/recorder/nightfactory/domain/Reservation.java
@@ -4,8 +4,10 @@ package org.recorder.nightfactory.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.LastModifiedDate;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.UUID;
 
@@ -38,10 +40,28 @@ public class Reservation {
     @Column(name = "numberOfPeople")
     private Integer numberOfPeople;
 
-    @Temporal(TemporalType.DATE)
     @Column(name = "reservationAt")
-    @LastModifiedDate
-    private Date reservationAt;
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    private LocalDateTime reservationAt;
+
+    @Column(columnDefinition = "BINARY(16)")
+    UUID id;
+
+    @Column(name = "imp_uid")
+    String impUid;
+
+    @Column(name = "amount")
+    Long amount;
+
+    @Column(name = "payment_state")
+    @Enumerated(EnumType.STRING)
+    PaymentState state;
+
+    @Column(name = "paid_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    LocalDateTime paidAt;
+
 
     public Reservation(Schedule schedule, String owner, Date reservationDate, String phoneNumber, int numberOfPeople) {
         this.schedule = schedule;
