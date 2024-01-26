852fbff4b23e6d04a533bf2958bca168c67c3637
BYUNJIHYUN <bjh8332@gmail.com>
2024-01-26 17:21:16 +0900

    footer 수정

--- src/main/java/org/recorder/nightfactory/controller/BoardController.java
+++ src/main/java/org/recorder/nightfactory/controller/BoardController.java
@@ -32,6 +32,6 @@ public class BoardController {
     @PostMapping("/post")
     public String write(BoardDTO boardDto) {
         boardService.savePost(boardDto);
-        return "redirect:/";
+        return "redirect:/list";
     }
 }
