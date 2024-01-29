e7f843f9d15671ea40ec16d011037fd445eca5e6
yeo-ok <wls990728@naver.com>
2024-01-29 16:36:20 +0900

    [refactor] Sms 인터페이스 구현

--- src/main/java/org/recorder/nightfactory/domain/Smsable.java
+++ src/main/java/org/recorder/nightfactory/domain/Smsable.java
@@ -1,6 +1,9 @@
 package org.recorder.nightfactory.domain;
 
+import net.nurigo.sdk.message.model.Message;
+
 public interface Smsable {
     public String getPhoneNumber();
     public String getMessageText();
+
 }
