9369c57bee259266713a694b3cd5b00c8b80f025
neul <is2eula@naver.com>
2024-04-17 15:41:35 +0900

    예약확인 버튼 input 필드 채운 후 활성화

--- docs/theme_dml.sql
+++ docs/theme_dml.sql
@@ -1,7 +1,7 @@
 INSERT INTO theme (room_id, name, description, difficulty, alt_difficulty, estimated_time, price, genre)
     VALUES (101,
             '관리실의 비밀 - 나폴리탄',
-            '밤의 공장을 관리하는 관리실에서 미스테리한 사건이 일어난다. 이 미스테리를 풀어야 공장에서 탈출할 수 있다. 안내문을 읽었는가? 행운을 빈다.',
+            '밤의 공장을 관리하는 관리실에서 미스테리한 사건이 일어난다.<br/> 이 미스테리를 풀어야 공장에서 탈출할 수 있다. 안내문을 읽었는가? 행운을 빈다.',
             5,
             'Level : Master',
             '90분',
@@ -11,7 +11,7 @@ INSERT INTO theme (room_id, name, description, difficulty, alt_difficulty, estim
     INSERT INTO theme (room_id, name, description, difficulty, alt_difficulty, estimated_time, price, genre)
     VALUES (102,
             '공장 작업실 - 살인사건',
-            '작업실에서 기이한 살인사건이 발생한다. 누가 범인인가? 살인자를 피해 작업실을 무사히 탈출하라',
+            '작업실에서 기이한 살인사건이 발생한다.<br/> 누가 범인인가? 살인자를 피해 작업실을 무사히 탈출하라',
             1,
             'Level : Easy',
             '80분',
@@ -21,7 +21,7 @@ INSERT INTO theme (room_id, name, description, difficulty, alt_difficulty, estim
     INSERT INTO theme (room_id, name, description, difficulty, alt_difficulty, estimated_time, price, genre)
     VALUES (103,
             '기계실: 피의 유령',
-            '기계실에서 유령을 봤다는 소문이 빠르게 직원들 사이에서 퍼지는데..유령의 정체를 밝혀내라 그리고 유령의 눈에서 벗어나라',
+            '기계실에서 유령을 봤다는 소문이 빠르게 직원들 사이에서 퍼지는데..<br/>유령의 정체를 밝혀내라 그리고 유령의 눈에서 벗어나라',
             2,
             'Level : Normal',
             '80분',
@@ -31,7 +31,7 @@ INSERT INTO theme (room_id, name, description, difficulty, alt_difficulty, estim
     INSERT INTO theme (room_id, name, description, difficulty, alt_difficulty, estimated_time, price, genre)
     VALUES (104,
             '기숙사 탈출',
-            '기괴한 일들이 기숙사에서 발생한다. 무엇을 상상해도 그 이상의 일들이 벌어질 것 살아남고 싶다면 반드시 탈출하라',
+            '기괴한 일들이 기숙사에서 발생한다.<br/> 무엇을 상상해도 그 이상의 일들이 벌어질 것 살아남고 싶다면 반드시 탈출하라',
             3,
             'Level : Hard',
             '80분',
